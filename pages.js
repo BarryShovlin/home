b3e599d41f3742a76f7f747be4699b22a9c98e13
Barry Shovlin <bshovlin32@gmail.com>
2021-12-29 13:51:57 -0600

    updated pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/barryshovlin/barryshovlin.github.io";
+const repoURL = "https://github.com/barryshovlin/barryshovlin.github.io.git";
 
 ghpages.publish(
   pathname,
